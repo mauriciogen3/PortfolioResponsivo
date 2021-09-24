ba3e4b67aa41f0809e733bb0108c6dc97aa8aa61
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-09-23 22:03:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-if__name__== '__main__':
-  app.run()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if__name__ == '__main__':
+    app.run()
