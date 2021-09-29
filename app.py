e5521d7300802c90982e0b7adc2c35de01c1aa01
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-09-28 22:51:50 -0300

    Contato configurado - Email PY

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash
 from flask_mail import Mail, Message
 from config import email, senha
 
@@ -29,6 +29,32 @@ class Contato:
 @app.route('/')
 def index():
     return render_template('index.html')
+
+    @app.route('/send', methods=['GET', 'POST'])
+    def send():
+        if request.method == 'POST':
+            formContato = Contato(
+                request.form["nome"],
+                request.form["email"],
+                request.form["mensagem"]
+            )
+
+            msg = Message(
+                subject=f'{formContato.nome} te  enviou uma mensagem no portfólio',
+                sender=app.config.ger("MAIL_USERNAME"),
+                recipients=['mauriciogen3@hotmail.com',
+                            app.config.ger("MAIL_USERNAME")]
+                body=f'''
+
+              {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte mensagem:
+              {formContato.mensagem}
+                
+                '''
+            )
+            mail.send(msg)
+
+            flash('Mensagem enviada com sucesso!')
+        return redirect('/')
 
 
 if __name__ == '__main__':
