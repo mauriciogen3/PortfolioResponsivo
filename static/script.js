beb2ecfafddb4786b0863ac3995f1230d5fdd152
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-02 00:24:40 -0300

    btn

--- static/script.js
+++ static/script.js
@@ -50,3 +50,12 @@ window.addEventListener("scroll", () => {
 //Ativar o carregamento do botao enviar
 
 const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener("click", () => {
+  btnEnviarLoader.style.display = "block";
+  btnEnviar.style.display = "none"
+})
+
+
+//Tirar a mensagem 
