91a2c991db6468f40182d855771f74a2275a2b04
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-02 02:05:21 -0300

    volta

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, redirect, request, flash
 from flask_mail import Mail, Message
-from dotenv import load_dotenv
-import os
-load_dotenv()
+from config import email, senha
 
 app = Flask(__name__)
 app.secret_key = 'mauricio'
@@ -12,8 +10,8 @@ mail_settings = {
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.getenv("EMAIL"),
-    "MAIL_PASSWORD": os.getenv("SENHA")
+    "MAIL_USERNAME": email,
+    "MAIL_PASSWORD": senha
 
 }
 
